a87acea6f83b4f561217244ccce13b9b65c04a77
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-06 19:00:16 +0530

    Refactor State Code CSV

---
+++ lib/src/test/java/DesignPrinciple/CensusAnalyzerStateCodeTest.java
@@ -0,0 +1,21 @@
+package DesignPrinciple;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class CensusAnalyzerStateCodeTest 
+{
+	private static final String INDIA_CENSUS_CSV_PATH = "C:\\Users\\susha\\Desktop\\DesignPrinciple\\lib\\src\\test\\resources\\IndiaStateCode.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        CensusAnalySer censusAnalyzer = new CensusAnalySer();
+        try {
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_PATH);
+            Assert.assertEquals(37, numOfRecord);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
+        }
+    }
+}

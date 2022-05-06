3d7922b969fb0483cfb858f8722efdb9aa8047f0
pihu2810 <vishaka.kadam01@gmail.com>
2022-05-06 18:41:12 +0530

    Refactor path is correct and type is different

--- lib/src/test/java/DesignPrinciple/CensusAnalyserTest.java
+++ lib/src/test/java/DesignPrinciple/CensusAnalyserTest.java
@@ -8,7 +8,7 @@ public class CensusAnalyserTest
 {
 	private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\susha\\Desktop\\DesignPrinciple\\lib\src\\test\\resources";
     private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\susha\\Desktop\\DesignPrinciple\\lib\\src\\test\\resources";
-
+    private static final String INDIA_CENSUS_CSV_WRONG_FILETYPE = "C:\\Users\\susha\\Desktop\\DesignPrinciple\\lib\\src\\test\\resources\\IndiaStateCensusData.pdf";
     @Test
     public void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -27,6 +27,17 @@ public class CensusAnalyserTest
             censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
         } catch (CensusAnalyserException e) {
             Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+    @Test
+    public void givenIndianCensusCSVFileReturnsInCorrecFileType_But_PathShouldBeCorrect() {
+    	try {
+        CensusAnalySer censusAnalyzer = new CensusAnalySer();
+       
+            int numOfRecord = censusAnalyzer.loadIndiaCensusData(INDIA_CENSUS_CSV_WRONG_FILETYPE);
+            Assert.assertEquals(29, numOfRecord);
+        } catch (CensusAnalyserException e) {
+            e.printStackTrace();
         }
     }
 
